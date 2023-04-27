f92de4be5b72811a4641aab715db92a59d3554a8
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-27 15:32:55 -0300

    ✨ feat: req 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
+import { useContext } from 'react';
+import Header from '../components/Header';
+import FilmsContext from '../context/FilmsContext';
+
 export default function Home() {
+  const { films } = useContext(FilmsContext);
   return (
-    <h1>Films</h1>
+    <>
+      <Header />
+      <h1>Films</h1>
+      {
+        films.map((film) => (
+          <div key={ film.id }>
+            <h2>{film.title}</h2>
+            <img src={ film.image } alt={ film.title } />
+            <p>{film.description}</p>
+            <button>Favoritar</button>
+          </div>
+        ))
+      }
+    </>
   );
 }
