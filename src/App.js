6cf430d6208e9d6a31c169bc37a10777723c6a1a
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-27 15:24:32 -0300

    ✨ feat: req 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { Route, Switch } from 'react-router-dom';
 
+import { useEffect, useState } from 'react';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
+import FilmsContext from './context/FilmsContext';
 
 function App() {
+  const [films, setFilms] = useState([]);
+
+  const fetchApi = async () => {
+    const API_URL = 'https://api-trybe-frontend.vercel.app/api/ghibli-animations';
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    setFilms(data);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
   return (
-    <Switch>
-      <Route exact path="/" component={ Home } />
-      <Route path="/favorites" component={ Favorites } />
-    </Switch>
+    <FilmsContext.Provider value={ { films } }>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/favorites" component={ Favorites } />
+      </Switch>
+    </FilmsContext.Provider>
   );
 }
 
