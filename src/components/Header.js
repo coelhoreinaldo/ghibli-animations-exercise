ea36aba1e2658c31b1c0d95a94e371ce7bc96b95
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-28 10:33:33 -0300

    🎨 feat: header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { Link, useLocation } from 'react-router-dom';
+import './Header.css';
+import logo from '../images/logo.svg';
 
 export default function Header() {
   const { pathname } = useLocation();
   return (
     <header>
+      <img src={ logo } alt="logo" />
       <nav>
         {pathname === '/favorites' ? (
           <Link to="/">Films</Link>
